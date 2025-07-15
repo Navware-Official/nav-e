bbdaab15ac4b42c56294f7f81b52637688710adc
a-navware <219419363+a-navware@users.noreply.github.com>
2025-07-16 00:57:44 +0200

    feat: begin search bar

--- app/lib/features/home/views/home_screen.dart
+++ app/lib/features/home/views/home_screen.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import '../widgets/search_bar_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-
-      body: Expanded(
-        child: Image.asset('assets/img/house_cat.jpeg',
-          width: double.infinity,
-          height: double.infinity,
-          fit: BoxFit.fitWidth,
-          alignment: Alignment.bottomCenter,
-        ),
+return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/img/house_cat.jpeg',
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.bottomCenter,
+            ),
+          ),
+          Positioned(
+            top: 40,
+            left: 16,
+            right: 16,
+            child: SearchBarWidget(),
+          ),
+        ],
       ),
 
       // This FAB floats above the bottom right
