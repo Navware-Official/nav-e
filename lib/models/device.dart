59fff30f004f66022e296309e33806f084ec5c30
s-navware <219421515+s-navware@users.noreply.github.com>
2025-07-31 00:22:30 +0200

    Added a basic device model

---
+++ lib/models/device.dart
@@ -0,0 +1,14 @@
+
+class Device {
+  final int id;
+  final String name;
+  final String model;
+  final String remoteId;
+
+  Device({
+    required this.id,
+    required this.name,
+    required this.model,
+    required this.remoteId,
+  });
+}
