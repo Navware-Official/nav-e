f0644c231559c49fc31595f946d4b8290d9e174f
s-navware <219421515+s-navware@users.noreply.github.com>
2025-08-02 00:56:28 +0200

    Added the base for bluetooth

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nav_e/app/app_router_delegate.dart';
 import 'package:nav_e/app/route_parser.dart';
 import 'package:nav_e/bloc/app_state_bloc.dart';
+import 'package:nav_e/bloc/bluetooth/bluetooth_bloc.dart';
 import 'package:nav_e/bloc/location_bloc.dart';
 import 'package:nav_e/bloc/map_bloc.dart';
 import 'package:nav_e/core/theme/app_theme.dart';
@@ -26,6 +27,9 @@ void main() {
         ),
         BlocProvider<SearchBloc>(
           create: (_) => SearchBloc(GeocodingService()),
+        ),
+        BlocProvider<BluetoothBloc>(
+          create: (_) => BluetoothBloc(),
         ),
       ],
       child: MaterialApp.router(
--- lib/screens/device_management_screen.dart
+++ lib/screens/device_management_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nav_e/bloc/app_state_bloc.dart';
+import 'package:nav_e/bloc/bluetooth/bluetooth_bloc.dart';
 
-class DeviceManagementScreen extends StatelessWidget {
+class DeviceManagementScreen extends StatefulWidget {
   const DeviceManagementScreen({super.key});
 
+  @override
+  State<DeviceManagementScreen> createState() => _DeviceManagementScreenState();
+}
+
+class _DeviceManagementScreenState extends State<DeviceManagementScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,9 +23,29 @@ class DeviceManagementScreen extends StatelessWidget {
         ),
         title: Text('Devices', style: TextStyle(color: Colors.black)),
       ),
-      body: Column(
-        children: [],
-      ),
+      body: 
+        BlocListener<BluetoothBloc, BluetoothState>(
+          listener: (context, state) {
+            if (state.isScanning) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(state.isScanning.toString()),
+                  duration: Duration(milliseconds: 300),
+                ),
+              );
+            }
+          },
+          child: Row(
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  context.read<BluetoothBloc>().add(StartScanning());
+                },
+                child: Text("Start Scanning"),
+              )
+            ],
+          ),
+        ),
     );
   }
 }
