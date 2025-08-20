f07e1127622c11f56a45602d28bff89b0e1428f5
s-navware <219421515+s-navware@users.noreply.github.com>
2025-08-20 15:34:22 +0200

    W.I.P.

--- lib/bloc/bluetooth/bluetooth_bloc.dart
+++ lib/bloc/bluetooth/bluetooth_bloc.dart
@@ -8,6 +8,9 @@ part 'bluetooth_state.dart';
 class BluetoothBloc extends Bloc<BluetoothEvent, BluetoothState> {
   final DatabaseHelper databaseHelper;
   BluetoothBloc(this.databaseHelper) : super(BluetoothInitial()) {
+
+    // TODO: create private check function that checks `if (await FlutterBluePlus.isSupported) {`
+    // TODO: Change tha NavigationStage to route to the "scanning" screen
     on<CheckBluetoothSupport>(_checkBluetoothSupport);
     on<CheckBluetoothAdapter>(_checkBluetoothAdapter);
     on<StartScanning>(_startScanning);
@@ -22,8 +25,37 @@ class BluetoothBloc extends Bloc<BluetoothEvent, BluetoothState> {
   }
 
   void _checkBluetoothAdapter(CheckBluetoothAdapter event, Emitter<BluetoothState> emit) async {
+    print("WE HERE BIIIIIIIIIIIIIIIIIIIIIIIIIISH");
+    // TODO: YOU LEFT OFF HEREEEEEE
+    // !  todo: MAYBE  CHECK THE CUBIT EXAMPLES ON HOW TO DO SUBS AND SHIT
+    // var subscription = FlutterBluePlus.adapterState.listen((BluetoothAdapterState state){
+    //   print("=====================");
+    //   print(state);
+
+    //   if (state == BluetoothAdapterState.on) {
+    //     print("DO IT GET HERE THO");
+    //     emit(BluetoothSupported());
+    //   } else {
+    //     print("WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH");
+    //     emit(BluetoothOperationFailure("Bluetooth Disabled. Please turn on and try again."));
+    //   }
+    // });
+
+    // // print("NIGGEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEER");
+
+    // subscription.cancel();
+    // return;
   }
 
   void _startScanning(StartScanning event, Emitter<BluetoothState> emit) async {
+    print("hihihihihihihihihihihihihihihihihihi");
+    // await databaseHelper.insertRow("devices", {"name": "vehicular manslaughter", "model": "First Edition", "remote_id": "0001"});
+    // await databaseHelper.insertRow("devices", {"name": "Ford Mustang", "model": "3.35", "remote_id": "foonnga"});
+    // await databaseHelper.insertRow("devices", {"name": "Mylyf", "model": "First Edition", "remote_id": "0001"});
+
+    // final items = await databaseHelper.getAllRowsFrom("devices");
+    // print(items);
+
+    // emit(state.copyWith(isScanning: true));
   }
 }
